c85a9c5ef53977aa7868d6e35f63c48af0919962
Alex Moreo <alejandro.moreo@isti.cnr.it>
2016-11-07 19:02:21 +0100

    classifier for 20 newsgroup in tensorflow

---
+++ src/20news_classifier.py
@@ -0,0 +1,146 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import tensorflow as tf
+from helpers import *
+from helpers import *
+from pprint import pprint
+
+
+class Dataset:
+    def __init__(self, valid_samples=1000, categories=None):
+        self.train = fetch_20newsgroups(subset='train', categories=categories)#,remove=('headers', 'footers', 'quotes'))
+        self.test  = fetch_20newsgroups(subset='test', categories=categories)#, remove=('headers', 'footers', 'quotes'))
+        vectorizer = TfidfVectorizer()
+        self.train_vec = vectorizer.fit_transform(self.train.data)
+        self.train_vec.sort_indices()
+        self.test_vec = vectorizer.transform(self.test.data)
+        self.test_vec.sort_indices()
+        self.valid_samples=valid_samples
+        self.epoch=0
+        self.offset=0
+
+    def valid_offset(self):
+        return self.num_tr_docs()-self.valid_samples
+
+    def get_index_values(self, batch):
+        num_indices = len(batch.nonzero()[0])
+        indices = batch.nonzero()[0].reshape(num_indices, 1)
+        values = batch.nonzero()[1]
+        return indices, values
+
+    def train_batch(self, batch_size=64):
+        to_pos = min(self.offset + batch_size, self.valid_offset())
+        batch = self.train_vec[self.offset:to_pos]
+        indices, values = self.get_index_values(batch)
+        labels = self.train.target[self.offset:to_pos]
+        self.offset = self.offset + batch_size
+        if self.offset >= self.valid_offset():
+            self.offset = 0
+            self.epoch += 1
+        return indices, values, labels
+
+    def val_batch(self):
+        batch = self.train_vec[self.valid_offset() : ]
+        indices, values = self.get_index_values(batch)
+        labels = self.train.target[self.valid_offset() : ]
+        return indices, values, labels
+
+    def test_batch(self):
+        indices, values = self.get_index_values(self.test_vec)
+        return indices, values, self.test.target
+
+    def num_tr_docs(self):
+        return len(self.train.filenames)
+
+    def num_categories(self):
+        return len(np.unique(self.train.target))
+
+    def num_features(self):
+        return self.train_vec.shape[1]
+
+def main(argv=None):
+    #categories = ['alt.atheism', 'talk.religion.misc', 'comp.graphics', 'sci.space']
+    categories = ['alt.atheism', 'talk.religion.misc']
+    data = Dataset(categories=categories)
+
+    x_size = data.num_features()
+    n_classes = data.num_categories()
+    embedding_size = 500
+    hidden_sizes = [1000]
+    drop_k_p=0.9
+
+    graph = tf.Graph()
+    with graph.as_default():
+      # Placeholders
+      x_indices = tf.placeholder(tf.int64, shape=[None, 1])
+      x_values = tf.placeholder(tf.int64, shape=[None])
+      x = tf.SparseTensor(indices=x_indices, values=x_values, shape=[3])
+      y = tf.placeholder(tf.int64, shape=[None])
+      embeddings = tf.Variable(tf.random_uniform([x_size, embedding_size], -1.0, 1.0))
+      keep_p = tf.placeholder(tf.float32, name='dropoupt_keep_p')
+
+      def add_layer(layer, hidden_size, drop, name):
+          weight, bias = projection_weights(layer.get_shape().as_list()[1], hidden_size, name=name)
+          activation = tf.nn.relu(tf.matmul(layer,weight)+bias)
+          if drop:
+              return tf.nn.dropout(activation, keep_prob=keep_p)
+          else:
+              return activation
+
+      doc_embed = tf.nn.embedding_lookup_sparse(embeddings, x, None)
+      current_layer = doc_embed
+      for i,hidden_dim in enumerate(hidden_sizes):
+          current_layer = add_layer(current_layer, hidden_dim, drop=True, name='layer'+str(i))
+      y_ = add_layer(current_layer, n_classes, drop=False, name='output_layer')
+
+      loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_,y))
+
+      correct_prediction = tf.equal(y, tf.argmax(y_, 1))
+      accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float")) * 100
+
+      op_step = tf.Variable(0, trainable=False)
+      rate = tf.train.exponential_decay(.1, op_step, 1, 0.9999)
+      optimizer = tf.train.GradientDescentOptimizer(learning_rate=rate).minimize(loss, global_step=op_step)
+
+
+    # ---------------------------------------------------------------------
+    # Graph run
+    # ---------------------------------------------------------------------
+
+    show_step = 100
+    valid_step = show_step * 10
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        l_ave=0.0
+        offset = 0
+        for step in range(1,1000000):
+            x_ind, x_val, y_lab = data.train_batch()
+            _,l,lr = session.run([optimizer, loss, rate], feed_dict={x_indices:x_ind, x_values:x_val,y:y_lab,keep_p:drop_k_p})
+            l_ave += l
+
+            if step % show_step == 0:
+                print('[step=%d][lr=%.5f] loss=%.7f' % (step, lr, l_ave / show_step))
+                l_ave = 0.0
+
+            if step % valid_step == 0:
+                x_ind, x_val, y_lab = data.val_batch()
+                #val_acc = tf.contrib.metrics.accuracy(predictions, y_lab)
+                val_acc = accuracy.eval(feed_dict={x_indices: x_ind, x_values: x_val, y: y_lab, keep_p: 1.0})
+                print('Validation accuracy %.3f%%' % val_acc)
+
+                x_ind, x_val, y_lab = data.test_batch()
+                test_acc = accuracy.eval(feed_dict={x_indices: x_ind, x_values: x_val, y: y_lab, keep_p: 1.0})
+                print('Test accuracy %.3f%%' % test_acc)
+
+
+
+
+
+#-------------------------------------
+if __name__ == '__main__':
+    flags = tf.app.flags
+    FLAGS = flags.FLAGS
+
+    tf.app.run()
--- src/learn_function.py
+++ src/learn_function.py
@@ -1,6 +1,9 @@
+from __future__ import division
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 from helpers import *
 import numpy as np
 import itertools
@@ -8,15 +11,11 @@ import itertools
 x_size=2
 
 
-def plot_coordinates():
-    x1_div = np.linspace(0.0, 1.0, 50)
-    x2_div = np.linspace(0.0, 1.0, 50)
+def plot_coordinates(div=50):
+    x1_div = np.linspace(0.0, 1.0, div)
+    x2_div = np.linspace(0.0, 1.0, div)
     points = itertools.product(x1_div, x2_div)
-    x1, x2 = [], []
-    for x1_, x2_ in points:
-        x1.append(x1_)
-        x2.append(x2_)
-    return x1,x2
+    return zip(*[p for p in points])
 
 def plot_function():
     x1,x2 = plot_coordinates()
@@ -24,32 +23,37 @@ def plot_function():
     plot_points(x1,x2,y)
 
 def plot_points(x1,x2,y):
-    if FLAGS.noplot:
-        return
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.plot_trisurf(x1, x2, y, linewidth=0.2)
     plt.show()
 
+def comp_plot(x1, x2, y_):
+    y = [target_function([x1[i], x2[i]]) for i in range(len(x1))]
+    fig = plt.figure(figsize=plt.figaspect(0.4))
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.set_title('Target function')
+    ax.plot_trisurf(x1, x2, y, linewidth=0.2, cmap=cm.jet)
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.set_title('Learnt function')
+    ax.plot_trisurf(x1, x2, y_, linewidth=0.2, cmap=cm.jet)
+    plt.show()
 
 def target_function(x):
     assert(len(x)==x_size)
-    return x[0]*x[1]
-
-plot_function()
-#sys.exit()
+    return 0.2+(x[0]**2+x[1])/2.0
 
 def sample():
     x = [random.random() for _ in range(x_size)]
     y = target_function(x)
     return (x,y)
 
-def batch(batch_size = 64):
+def batch(batch_size = 100):
     next = [sample() for _ in range(batch_size)]
     return zip(*next)
 
 def main(argv=None):
-    hidden_size = 10
+    hidden_size = 30
 
     graph = tf.Graph()
     with graph.as_default():
@@ -59,50 +63,56 @@ def main(argv=None):
 
       # Model parameters
       w1, b1 = projection_weights(x_size, hidden_size, 'x-hidden')
-      #w2, b2 = projection_weights(hidden_size, hidden_size, 'hidden-hidden')
-      w3, b3 = projection_weights(hidden_size, 1, 'hidden-y')
+      w2, b2 = projection_weights(hidden_size, hidden_size, 'hidden-hidden')
+      w3, b3 = projection_weights(hidden_size, hidden_size, 'hidden-hidden')
+      w4, b4 = projection_weights(hidden_size, 1, 'hidden-y')
 
-      h1 = tf.nn.sigmoid(tf.matmul(x,w1)+b1)
-      #h2 = tf.nn.relu(tf.matmul(h1,w2)+b2)
-      _y = tf.nn.sigmoid(tf.matmul(h1,w3)+b3)
+      h1 = tf.nn.relu(tf.matmul(x,w1)+b1)
+      h2 = tf.nn.relu(tf.matmul(h1,w2)+b2)
+      h3 = tf.nn.relu(tf.matmul(h2, w3) + b3)
+      _y = (tf.matmul(h3,w4)+b4)
+      #_y = tf.nn.sigmoid(tf.matmul(x, w1)+b1)
 
-      loss = tf.reduce_mean(tf.square(y - _y))
+      #reg = (tf.nn.l2_loss(w1)+tf.nn.l2_loss(b1))/hidden_size
+      def uni_reg(w):
+          cov = tf.matmul(tf.transpose(w),w)
+          cov = cov - tf.diag(cov)
+          return tf.nn.l2_loss(cov)/w.get_shape().as_list()[1]
 
-      optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
+      r = 0.0*(uni_reg(w1)+uni_reg(w2)+uni_reg(w3))
 
-      # ----------------------------------------------------------------------------------------------
-      # Add ops to save and restore all the variables.
-      saver = tf.train.Saver(max_to_keep=1)
+      #loss = tf.sqrt(tf.reduce_mean(tf.square(tf.sub(y, _y)))) #+ reg
+      loss = tf.reduce_mean(tf.abs(tf.sub(y, _y))) #+ r
+
+      op_step = tf.Variable(0, trainable=False)
+      rate = tf.train.exponential_decay(.0001, op_step, 1, 0.9999)
+      optimizer = tf.train.GradientDescentOptimizer(learning_rate=rate).minimize(loss, global_step=op_step)
+
 
     # ---------------------------------------------------------------------
     # Graph run
     # ---------------------------------------------------------------------
 
-    show_step = 10
-    valid_step = 2500
+    show_step = 100
+    valid_step = 10000
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
         l_ave=0.0
         for step in range(1,1000000):
-            #x_, y_ = batch()
-            #_,l = session.run([optimizer, loss], feed_dict={x:x_,y:y_})
-            #l_ave += l
-
-            x1_, x2_ = plot_coordinates()
-            x_ = zip(x1_, x2_)
-            y_ = [target_function([x1_[i], x2_[i]]) for i in range(len(x1_))]
-            _, l = session.run([optimizer, loss], feed_dict={x: x_, y: y_})
+            x_, y_ = batch()
+            _,l,lr = session.run([optimizer, loss, rate], feed_dict={x:x_, y:y_})
             l_ave += l
 
             if step % show_step == 0:
-                print('[step=%d] loss=%.4f' % (step, l_ave / show_step))
+                print('[step=%d][lr=%.5f] loss=%.7f [r=%.7f]' % (step, lr, l_ave / show_step, -1))
                 l_ave = 0.0
 
-            if step % valid_step == 0:
-                x1_, x2_ = plot_coordinates()
+            if step % valid_step == 0 and FLAGS.plot:
+                x1_, x2_ = plot_coordinates(div=10)
                 y_ = _y.eval(feed_dict={x: zip(x1_, x2_)})
                 y_ = np.reshape(y_,len(x1_))
-                plot_points(x1_, x2_, y_)
+                comp_plot(x1_,x2_,y_)
+
 
 
 #-------------------------------------
@@ -112,5 +122,6 @@ if __name__ == '__main__':
 
     flags.DEFINE_boolean('plot', True, 'Plots the current function model.')
 
+    #plot_function()
     tf.app.run()
 
