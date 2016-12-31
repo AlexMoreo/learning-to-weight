00dd9a5728e767424c9f7c8238c04e12781e6d3c
Alex Moreo <alejandro.moreo@isti.cnr.it>
2016-12-31 13:44:32 +0100

    batch script for imdb

--- src/jobs.sh
+++ src/jobs.sh
@@ -5,26 +5,33 @@ exe='python supervised_weighting.py'
 mplot='--plotmode img'
 batch='--batchsize 1000'
 
-for dataset in 'ohsumed'
+for dataset in 'imdb'
 do
-dresult='--resultcontainer ../results/newresults.csv'
-for run in {0..9}
-do
-	for cat in {0..23}
-	do
-		log=../logs/cat"$cat".txt
-		dplot=../plot/"$dataset"/C"$cat"
-		#$exe --dataset $dataset --cat "$cat" --normalize --pretrain off --forcepos $batch $dresult $mplot --plotdir "$dplot"/norm_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
-		#$exe --dataset $dataset --cat "$cat" --nonormalize --pretrain off --forcepos $batch $dresult $mplot --plotdir "$dplot"/nonorm_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
-		$exe --dataset $dataset --cat "$cat" --normalize --pretrain off --noforcepos $batch $dresult $mplot --plotdir "$dplot"/norm_posneg --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
-		#$exe --dataset $dataset --cat "$cat" --nonormalize --pretrain off --noforcepos $batch $dresult $mplot --plotdir "$dplot"/nonorm_posneg --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
-		#$exe --dataset $dataset --cat "$cat" --normalize --pretrain infogain --forcepos $batch $dresult $mplot --plotdir "$dplot"/norm_pretraininfogain_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
-	done
+    dresult='--resultcontainer ../results/newresults.csv'
+    for run in {0..4}
+    do
+    #	for cat in {0..23}
+        for cat in 1
+        do
+            log=../logs/cat"$cat".txt
+            dplot=../plot/"$dataset"/C"$cat"
+            $exe --dataset $dataset --cat "$cat" --normalize --forcepos --linidf $batch $dresult $mplot --plotdir "$dplot"/norm_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            $exe --dataset $dataset --cat "$cat" --normalize --noforcepos --linidf $batch $dresult $mplot --plotdir "$dplot"/norm_nopos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            $exe --dataset $dataset --cat "$cat" --normalize --forcepos --nolinidf $batch $dresult $mplot --plotdir "$dplot"/norm_pos_nolin --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            $exe --dataset $dataset --cat "$cat" --normalize --noforcepos --nolinidf $batch $dresult $mplot --plotdir "$dplot"/norm_nopos_nolin --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+
+
+            #$exe --dataset $dataset --cat "$cat" --nonormalize --pretrain off --forcepos $batch $dresult $mplot --plotdir "$dplot"/nonorm_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            #$exe --dataset $dataset --cat "$cat" --normalize --pretrain off --noforcepos $batch $dresult $mplot --plotdir "$dplot"/norm_posneg --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            #$exe --dataset $dataset --cat "$cat" --nonormalize --pretrain off --noforcepos $batch $dresult $mplot --plotdir "$dplot"/nonorm_posneg --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+            #$exe --dataset $dataset --cat "$cat" --normalize --pretrain infogain --forcepos $batch $dresult $mplot --plotdir "$dplot"/norm_pretraininfogain_pos --outdir ../vectors/"$dataset" --run "$run" |& tee -a $log
+        done
+
+    done
 
 done
-done
 
-#python classification_benchmark.py -v ../vectors/reuters21578/ --no-randomforest -r ../results/reuters21578_at01.csv
+python classification_benchmark.py -v ../vectors/"$dataset" --no-randomforest --no-multinomialnb --no-randomforest -r ../results/"$dataset"_at01.csv
 
 
 
