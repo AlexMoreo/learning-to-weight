cad7dbd3a67bdc5e817d2adba3c8bd0df625494b
Alex Moreo <alejandro.moreo@isti.cnr.it>
2017-01-30 18:04:50 +0100

    different version for te tf-like computation, lighter

--- src/multi_supervised_weighting.py
+++ src/multi_supervised_weighting.py
@@ -73,11 +73,11 @@ def main(argv=None):
         feat_info = tf.constant(np.concatenate(feat_corr_info), dtype=tf.float32)
 
         def tf_like(x_raw):
-            #tf_pow = tf.get_variable('tf_pow', shape=[1], initializer=tf.constant_initializer(1.0))
-            #tf_prod = tf.get_variable('tf_prod', shape=[1], initializer=tf.constant_initializer(1.0))
-            #tf_offset = tf.get_variable('tf_sum', shape=[1], initializer=tf.constant_initializer(0.0))
+            tf_pow = tf.get_variable('tf_pow', shape=[1], initializer=tf.constant_initializer(1.0))
+            tf_prod = tf.get_variable('tf_prod', shape=[1], initializer=tf.constant_initializer(1.0))
+            tf_offset = tf.get_variable('tf_sum', shape=[1], initializer=tf.constant_initializer(0.0))
 
-            #return tf.add(tf.mul(tf.pow(x_raw, tf_pow), tf_prod), tf_offset)
+            return tf.add(tf.mul(tf.pow(x_raw, tf_pow), tf_prod), tf_offset)
             #-------
             width = 1
             in_channels = 1
