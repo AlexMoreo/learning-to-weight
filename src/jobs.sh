6e3c897455342d2702dcbb97ff1637a7bf182903
Alex Moreo <alejandro.moreo@isti.cnr.it>
2017-02-22 20:01:57 +0100

    jobs.sh script

--- src/jobs.sh
+++ src/jobs.sh
@@ -3,29 +3,24 @@ set -x
 
 exe='python classification_benchmark.py'
 dataset ='-d reuters21578'
-result = '-r ../results/baselines_reuters_svm_multiclass.csv'
+result = '-r ../results/baselines_svm.csv'
 
-$exe $dataset $result -m binary --fs 0.1 --classification multiclass
-$exe $dataset $result -m tf --fs 0.1 --classification multiclass
-$exe $dataset $result -m tf --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m tfidf --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfidf --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m bm25 --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfig --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfig --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m tfcw --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfcw --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m tfrf --fs 0.1 --classification multiclass
-$exe $dataset $result -m tfrf --fs 0.1 --classification multiclass --sublinear_tf
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --global_policy max
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --sublinear_tf --global_policy max
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --global_policy sum
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --sublinear_tf --global_policy sum
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --global_policy ave
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --sublinear_tf --global_policy ave
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --global_policy wave
-$exe $dataset $result -m tfgr --fs 0.1 --classification multiclass --sublinear_tf --global_policy wave
+for dataset in 'ohsumed' '20newsgroups' 'reuters21578'
+do
+    $exe $dataset $result -m binary --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tf --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tf --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m tfidf --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfidf --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m bm25 --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfig --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfig --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m tfcw --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfcw --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m tfrf --fs 0.1 --classification multiclass
+    $exe $dataset $result -m tfrf --fs 0.1 --classification multiclass --sublinear_tf
+done
 
 
