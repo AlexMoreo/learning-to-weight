aefcd4d5801d679219db631213ec3114d2ae01bf
Alex Moreo <alejandro.moreo@isti.cnr.it>
2017-02-22 20:04:25 +0100

    jobs.sh second try

--- src/jobs.sh
+++ src/jobs.sh
@@ -2,25 +2,26 @@
 set -x
 
 exe='python classification_benchmark.py'
-dataset ='-d reuters21578'
-result = '-r ../results/baselines_svm.csv'
+dataset='-d reuters21578'
+result='-r ../results/baselines_svm.csv'
+classification='--classification binary'
 
 for dataset in 'ohsumed' '20newsgroups' 'reuters21578'
 do
-    $exe $dataset $result -m binary --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tf --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tf --fs 0.1 --classification multiclass --sublinear_tf
-    $exe $dataset $result -m tfidf --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfidf --fs 0.1 --classification multiclass --sublinear_tf
-    $exe $dataset $result -m bm25 --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfig --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfig --fs 0.1 --classification multiclass --sublinear_tf
-    $exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfchi2 --fs 0.1 --classification multiclass --sublinear_tf
-    $exe $dataset $result -m tfcw --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfcw --fs 0.1 --classification multiclass --sublinear_tf
-    $exe $dataset $result -m tfrf --fs 0.1 --classification multiclass
-    $exe $dataset $result -m tfrf --fs 0.1 --classification multiclass --sublinear_tf
+    $exe $dataset $result -m binary --fs 0.1 $classification
+    $exe $dataset $result -m tf --fs 0.1 $classification
+    $exe $dataset $result -m tf --fs 0.1 $classification --sublinear_tf
+    $exe $dataset $result -m tfidf --fs 0.1 $classification
+    $exe $dataset $result -m tfidf --fs 0.1 $classification --sublinear_tf
+    $exe $dataset $result -m bm25 --fs 0.1 $classification
+    $exe $dataset $result -m tfig --fs 0.1 $classification
+    $exe $dataset $result -m tfig --fs 0.1 $classification --sublinear_tf
+    $exe $dataset $result -m tfchi2 --fs 0.1 $classification
+    $exe $dataset $result -m tfchi2 --fs 0.1 $classification --sublinear_tf
+    $exe $dataset $result -m tfcw --fs 0.1 $classification
+    $exe $dataset $result -m tfcw --fs 0.1 $classification --sublinear_tf
+    $exe $dataset $result -m tfrf --fs 0.1 $classification
+    $exe $dataset $result -m tfrf --fs 0.1 $classification --sublinear_tf
 done
 
 
