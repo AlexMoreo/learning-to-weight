da23a738903e5baa3b5ab7e8a3c7ba097cbe4bbe
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2017-02-23 17:01:33 +0100

    run scripts

--- src/light_jobs.sh
+++ src/light_jobs.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 set -x
 
-exe='python light_supervised_weighting.py'
+exe='python simple_supervised_weighting.py '
 
 for dataset in 'ohsumed' '20newsgroups' 'reuters21578'
 do
-    for run in 0
+    for cat in {0..115}
     do
-    	for cat in {0..115}
-        do
-            log=../logs/cat"$cat".txt
-            for idf in 'infogain' 'chisquare' 'gainratio' 'idf' 'rel_factor'
-            do
-
-                $exe --dataset $dataset --cat "$cat" --outdir ../vectors/"$dataset"_light --idflike $idf --run "$run"
-
-            done
-        done
+        $exe -v ../vectors -r ../results/simple_learn_svm.csv -d $dataset --cat "$cat"
     done
-    python classification_benchmark.py -v ../vectors/"$dataset"_light --no-randomforest -r ../results/"$dataset"_light.csv
+
+    python classification_benchmark.py -v ../vectors -r ../results/simple_learn_svm.csv
 done
