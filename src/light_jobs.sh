573d024af14e8c09d2f7f5c52a7776319fba03ac
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2017-02-24 19:37:00 +0100

    updated batch scripts

--- src/light_jobs.sh
+++ src/light_jobs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 
-exe='python simple_supervised_weighting.py '
+exe='python simple_supervised_weighting.py'
 
 for dataset in 'ohsumed' '20newsgroups' 'reuters21578'
 do
     for cat in {0..115}
     do
-        $exe -v ../vectors -r ../results/simple_learn_svm.csv -d $dataset --cat "$cat"
+        $exe --dataset "$dataset" --cat "$cat" --fs 0.1
     done
 
     python classification_benchmark.py -v ../vectors -r ../results/simple_learn_svm.csv
