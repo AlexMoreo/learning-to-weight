f6564c079cc0784a1d9096472cdbdfdd8069fd56
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2017-06-14 08:56:44 +0200

    probando cosas con ri

--- src/future_work/random_indexing.py
+++ src/future_work/random_indexing.py
@@ -1,3 +1,4 @@
+import dill
 import math
 import random as rn
 import numpy as np
@@ -72,3 +73,117 @@ class RandomIndexing(object):
         return self.count_nonzeros(matrix)*1.0/ np.prod(matrix.shape)
 
 
+import sys
+import math
+from sklearn import random_projection
+import random as rn
+#def warn(*args, **kwargs):
+#    pass
+#import warnings
+#warnings.warn = warn
+import numpy as np
+from data.dataset_loader import TextCollectionLoader
+from sklearn.feature_extraction.text import *
+from sklearn.svm import SVC, LinearSVC
+from scipy import stats
+from scipy.spatial.distance import cdist
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+from sklearn.multiclass import OneVsRestClassifier
+from utils.metrics import *
+from data.dataset_loader import TextCollectionLoader
+
+def fit_model_hyperparameters(data, parameters, model):
+    single_class = data.num_categories() == 1
+    if not single_class:
+        parameters = {'estimator__' + key: parameters[key] for key in parameters.keys()}
+        model = OneVsRestClassifier(model, n_jobs=-1)
+    model_tunning = GridSearchCV(model, param_grid=parameters,
+                                 scoring=make_scorer(macroF1), error_score=0, refit=True, cv=3, n_jobs=-1)
+
+    Xtr, ytr = data.get_devel_set()
+    Xtr.sort_indices()
+    if single_class:
+        ytr = np.squeeze(ytr)
+
+    tunned = model_tunning.fit(Xtr, ytr)
+
+    return tunned
+
+def fit_and_test_model(data, parameters, model):
+    init_time = time.time()
+    tunned_model = fit_model_hyperparameters(data, parameters, model)
+    tunning_time = time.time() - init_time
+    print("%s: best parameters %s, best score %.3f, took %.3f seconds" %
+          (type(model).__name__, tunned_model.best_params_, tunned_model.best_score_, tunning_time))
+
+    Xte, yte = data.get_test_set()
+    Xte.sort_indices()
+    yte_ = tunned_model.predict(Xte)
+
+    macro_f1 = macroF1(yte, yte_)
+    micro_f1 = microF1(yte, yte_)
+    print("Test scores: %.3f macro-f1, %.3f micro-f1" % (macro_f1, micro_f1))
+#print "classification / test"
+def linear_svm(data):
+    parameters = {'C': [1e2, 1e1, 1],
+                  'loss': ['squared_hinge'],
+                  'dual': [True, False]}
+    model = LinearSVC()
+    fit_and_test_model(data, parameters, model)
+
+def learner_without_gridsearch(data, learner):
+    model = OneVsRestClassifier(learner, n_jobs=7)
+
+    Xtr, ytr = data.get_devel_set()
+    trained_model = model.fit(Xtr, ytr)
+
+    Xte, yte = data.get_test_set()
+    yte_ = trained_model.predict(Xte)
+
+    macro_f1 = macroF1(yte, yte_)
+    micro_f1 = microF1(yte, yte_)
+    print("Test scores: %.3f macro-f1, %.3f micro-f1" % (macro_f1, micro_f1))
+
+data = TextCollectionLoader(dataset='reuters21578')
+data.devel.target = data.devel.target[:,:10]
+data.test.target = data.test.target[:,:10]
+
+print("\nwith bow and linear kernel")
+learner_without_gridsearch(data, LinearSVC())
+
+nD = data.num_devel_documents()
+nF = data.num_features()
+nR = nF
+non_zeros = 2 # nF*0.01
+random_indexing = RandomIndexing(latent_dimensions=nR, non_zeros=non_zeros, positive=False)
+
+data.devel_vec  = random_indexing.fit_transform(data.devel_vec)
+data.test_vec   = random_indexing.transform(data.test_vec)
+
+P = random_indexing.projection_matrix
+R = np.dot(P.T,P)
+ri_kernel = lambda X, Y: np.dot(np.dot(X, R), Y.T).toarray()
+
+print("\nwith linear kernel")
+learner_without_gridsearch(data, LinearSVC())
+
+print("\nwith self-covariance kernel")
+learner_without_gridsearch(data, SVC(kernel=ri_kernel))
+
+#
+
+
+#BoW   Test scores: 0.564 macro-f1, 0.855 micro-f1
+#RI 1% Test scores: 0.559 macro-f1, 0.848 micro-f1
+#LRI   Test scores: 0.542 macro-f1, 0.842 micro-f1
+#LRI K:Test scores: 0.565 macro-f1, 0.842 micro-f1
+
+# with bow and linear kernel
+# Test scores: 0.564 macro-f1, 0.855 micro-f1
+#
+# with linear kernel
+# Test scores: 0.561 macro-f1, 0.855 micro-f1
+#
+# with self-covarianze kernel
+# Test scores: 0.578 macro-f1, 0.859 micro-f1
