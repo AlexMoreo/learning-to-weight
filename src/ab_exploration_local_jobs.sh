5651eab3764124f070df97d83c00aa9a77c43dfa
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2017-08-03 11:32:44 +0200

    script modified

--- src/ab_exploration_local_jobs.sh
+++ src/ab_exploration_local_jobs.sh
@@ -3,23 +3,22 @@ set -x
 
 #this experiment do not explore the parameter C of the svm. Instead it performs 5-fold cross-validation (the previous was 3-fold cv)
 
-for dataset in 'reuters21578' '20newsgroups' 'ohsumed20k'
+for dataset in 'ohsumed20k' '20newsgroups' 'reuters21578'
 do
-    for vect in 'tfig' 'tfgr' 'tfchi' 'tfrf' 'tfcw'
+    for vect in 'tfgss' 'tfpmi' 'tfigpos' #tfcw' 'tfrf' 'tfchi' 'tfgr' 'tfig'
     do
+        python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 0
+        python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv --sublinear_tf -l LinearSVC --params 0
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 1
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv --sublinear_tf -l LinearSVC --params 1
     done
 
-done
-
-
-for dataset in 'reuters21578' '20newsgroups' 'ohsumed20k'
-do
-    for vect in 'tfig' 'tfgr' 'tfchi' 'tfrf' 'tfcw'
-    do
-        python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 2
-        python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv --sublinear_tf -l LinearSVC --params 2
-    done
+    #python ab_exploration_local.py -d "$dataset" -v tfidf -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 0
+    #python ab_exploration_local.py -d "$dataset" -v tfidf -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 0 --sublinear_tf
+    #python ab_exploration_local.py -d "$dataset" -v bm25 -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 0
+    #python ab_exploration_local.py -d "$dataset" -v tfidf -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 1
+    #python ab_exploration_local.py -d "$dataset" -v tfidf -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 1 --sublinear_tf
+    #python ab_exploration_local.py -d "$dataset" -v bm25 -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 1
 
 done
+
