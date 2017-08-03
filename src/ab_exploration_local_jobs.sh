a63a63e595300201f9589123c50487f390288f5c
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2017-08-03 11:31:14 +0200

    preventing power issues in tsr that allow for negative values

--- src/ab_exploration_local_jobs.sh
+++ src/ab_exploration_local_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 
-#this experiment do not explore the parameter C of the svm. Instead it performs 5-fold cross-validation (the previous was 3-fold cv)
+# 'tfgr' is not neccessary in local as it is equivalent to tfig
 
 for dataset in 'reuters21578' '20newsgroups' 'ohsumed20k'
 do
-    for vect in 'tfig' 'tfgr' 'tfchi' 'tfrf' 'tfcw'
+    for vect in 'tfig' 'tfchi' 'tfrf' 'tfcw'
     do
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 1
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv --sublinear_tf -l LinearSVC --params 1
@@ -16,7 +16,7 @@ done
 
 for dataset in 'reuters21578' '20newsgroups' 'ohsumed20k'
 do
-    for vect in 'tfig' 'tfgr' 'tfchi' 'tfrf' 'tfcw'
+    for vect in 'tfig' 'tfchi' 'tfrf' 'tfcw'
     do
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv -l LinearSVC --params 2
         python ab_exploration_local.py -d "$dataset" -v "$vect" -r ../results/alphabeta/ab_local.csv --sublinear_tf -l LinearSVC --params 2
