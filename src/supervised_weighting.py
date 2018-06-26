7bfd94a0e8da2838bb77eb5c384cd88ddb86fee1
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2018-06-26 10:32:43 +0200

    bug

--- src/supervised_weighting.py
+++ src/supervised_weighting.py
@@ -68,7 +68,7 @@ def main(argv=None):
                 h2tf, h2tf_b = get_projection_weights([FLAGS.hiddentf, 1], 'h2tf_weights')
                 tf_tensor = tf.reshape(x_raw, shape=[-1, 1])
                 tf_hidden = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(tf.matmul(tf_tensor, tf2h), tf2h_b)), keep_prob=keep_p)
-                tf_factor = tf.nn.bias_add(tf.matmul(tf_hidden, h2tf), h2tf_b)
+                tf_factor = tf.nn.relu(tf.nn.bias_add(tf.matmul(tf_hidden, h2tf), h2tf_b))
                 tf_factor = tf.reshape(tf_factor, shape=[-1, x_size])
                 tf_factor = tf.multiply(tf_factor, tf.sign(x_raw)) # guarantees dd(t,d)=0 if t not in d
             else:
